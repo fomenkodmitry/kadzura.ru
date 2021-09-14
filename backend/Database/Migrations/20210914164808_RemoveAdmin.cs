d2f9be3768a174ccfb1896cf9de55b21c8144825
Dmitriy Fomenko <dfomenko@mmtr.ru>
2021-09-14 19:53:56 +0300

    fix

---
+++ backend/Database/Migrations/20210914164808_RemoveAdmin.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Database.Migrations
+{
+    public partial class RemoveAdmin : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Admins");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Admins",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    AdminGuid = table.Column<string>(type: "TEXT", nullable: true),
+                    DateCreation = table.Column<long>(type: "INTEGER", nullable: false),
+                    Login = table.Column<string>(type: "TEXT", nullable: true),
+                    Password = table.Column<string>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Admins", x => x.Id);
+                });
+        }
+    }
+}
