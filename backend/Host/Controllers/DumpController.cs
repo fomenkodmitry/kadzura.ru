5917cab006a008cbe6caf1572998dc0e449d3a5c
Dmitriy Fomenko <dfomenko@mmtr.ru>
2021-11-01 12:22:08 +0300

    Download dump

---
+++ backend/Host/Controllers/DumpController.cs
@@ -0,0 +1,27 @@
+﻿using System.IO;
+using System.Threading.Tasks;
+using Kadzura.Web.Extensions;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Host.Controllers
+{
+    /// <summary>
+    /// </summary>
+    [Route("api/v{version:apiVersion}/dump")]
+    [ApiVersion("1")]
+    [ApiController]
+    [Authorize]
+    public class DumpController : ControllerBase
+    {
+
+        [HttpGet]
+        [ProducesResponseType(typeof(FileResult), StatusCodes.Status200OK)]
+        [ApiVersionRange(1)]
+        public async Task<IActionResult> Get()
+        {
+            return File(new FileStream("Kadzura.db", FileMode.Open), "application/vnd.sqlite3", "Kadzura.db");
+        }
+    }
+}
